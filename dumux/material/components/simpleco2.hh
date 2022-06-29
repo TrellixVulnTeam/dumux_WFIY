d389796cbd99f5593cda10d6b161dc1a8df22c12
Mathis Kelm <mathis.kelm@iws.uni-stuttgart.de>
2022-06-29 14:42:32 +0200

    [Components][CO2] default back to non-aqueous component

--- dumux/material/components/simpleco2.hh
+++ dumux/material/components/simpleco2.hh
@@ -262,9 +262,6 @@ public:
 };
 
 template <class Scalar>
-struct IsAqueous<SimpleCO2<Scalar>> : public std::true_type {};  //TODO Do we need this actually? What does it do?
-
-template <class Scalar>
 const Scalar Components::SimpleCO2<Scalar>::R = Constants<Scalar>::R / Components::SimpleCO2<Scalar>::molarMass();
 
 } // end namespace Dumux::Components
